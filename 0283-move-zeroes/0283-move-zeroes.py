b42354329db8ab25644e09418a013075f681ea0d
shalu891 <shalugiri964@gmail.com>
2026-07-28 21:14:13 +0530

    Time: 9 ms (32.43%), Space: 13.7 MB (19.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,18 @@
 class Solution:
     def moveZeroes(self, nums):
-        snowBallSize = 0
-        for i in range(0, len(nums)):
-            if nums[i] == 0:
-                snowBallSize += 1
-            elif snowBallSize > 0:
-                t = nums[i]
-                nums[i] = 0
-                nums[i - snowBallSize] = t
+        n = len(nums)
+        temp = []
+
+        for i in range(0, n):
+            if nums[i] != 0:
+                temp.append(nums[i])
+
+        nZ = len(temp)
+        for i in range(0, nZ):
+            nums[i] = temp[i]
+
+        for i in range(nZ, n):
+            nums[i] = 0
+       
+
+
