669ec12f7799f92c68f613de64eeca166a91c6df
shalu891 <shalugiri964@gmail.com>
2026-08-23 21:22:06 +0530

    Time: 0 ms (100%), Space: 12.2 MB (90.48%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x < 2:
+            return x
+        
+        left, right = 1, x // 2
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            square = mid * mid
+            
+            if square == x:
+                return mid
+            elif square < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return right
+        
