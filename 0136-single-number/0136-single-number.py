63cdd961fc4858f303666e0a00f89170f1208a0b
shalu891 <shalugiri964@gmail.com>
2026-09-10 20:13:41 +0530

    Time: 0 ms (100%), Space: 13.9 MB (52.75%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        ans = 0
+
+        for num in nums:       
+            ans = ans ^ num    
+
+        return ans
+        
+        
